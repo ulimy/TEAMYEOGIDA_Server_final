e4d0d9b03cc6f976ee8539550c148c53096af5cc
ulimy <ulim130@naver.com>
2018-08-20 22:41:07 +0900

    login

--- yeogida/models/login.js
+++ yeogida/models/login.js
@@ -1,34 +1,16 @@
 //넘겨받은 json에서 userpid 검색
-var db = require("../config");
-var mysql= require("mysql");
+var profile = require("../config");
+//var mysql= require("mysql");
 
-exports.login= (login_info) => {
-  return new Promise((resolve,reject) => {
-    var select = "select personpid from profile where kakaopid='"+login_info[3]+"';";
-    var insert = "INSERT INTO profile(phone,kakaonickname,email,kakaopid) VALUES (?,?,?,?);";
-    db.query(select,function(err,result){
-      if (err) reject(err);
-
-      // 회원정보가 없으면 profile에 추가
-      if (!result.length){
-        console.log("not exist");
-        db.query(insert,login_info,function(err){
-          if (err) reject(err);
-          // 추가한 회원의 personpid 찾기
-          db.query(select,function(err,rows){
-              if (err) reject(err);
-            personpid = rows[0].personpid;
-            resolve(personpid);
-          });
-        });
-      // 회원정보가 있으면 그 회원의 personpid 찾기
-      }else{
-        console.log("exist");
-        personpid = result[0].personpid;
-
-        resolve(personpid);
-      }
-    });
-
+exports.login = (login_info) => profile.findOrCreate({
+    where : login_info
+  }).spread((user,created) =>{
+    if (created){
+      console.log("created");
+    }
+    return user.get();
   });
-};
+
+exports.savetoken = (personpid,token) =>profile.update({token : token},
+  {where : {personpid : personpid}}
+  )
