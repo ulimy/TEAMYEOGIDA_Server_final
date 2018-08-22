ec15ab08da1e1c3651e10bdf52f01659bb276fca
ulimy <ulim130@naver.com>
2018-08-22 19:28:02 +0900

    figure out form-data

--- yeogida/controllers/product.js
+++ yeogida/controllers/product.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var client_id = '';
 var client_secret = '';
 var request = require('request');
+var multer = require('multer');
+var upload = multer();
 
 // 제품 상세조회
-router.post('/info',function(req,res){
+router.post('/info',upload.fields([]),function(req,res){
   var infoModel = require('../models/product_info');
   // 최근 본 상품 추가
   infoModel.insert_search(req.body);
@@ -19,7 +21,7 @@ router.post('/info',function(req,res){
 });
 
 // 제품 등록
-router.post('/register',function(req,res){
+router.post('/register',upload.fields([]),function(req,res){
   var registerModel = require('../models/product_register');
   var map=require('../api/map_api');
   var register_info=req.body;
@@ -38,10 +40,11 @@ router.post('/register',function(req,res){
 });
 
 // 제품 수정
-router.post('/update',function(req,res){
+router.post('/update',upload.fields([]),function(req,res){
   var updateModel = require('../models/product_update');
   var map=require('../api/map_api');
   var update_info=req.body;
+  console.log(update_info.productaddress);
   // 좌표값 변환하여 productinfo 에 저장
   map.getPoint(update_info.productaddress).then(function(point){
     update_info.productaddress_x = point[0];
@@ -49,21 +52,19 @@ router.post('/update',function(req,res){
     var productpid = update_info.productpid;
     delete update_info.productpid;
     updateModel.update(update_info,productpid);
-  });
+   });
 });
 
 // 제품 삭제
-router.get('/delete',function(req,res){
+router.get('/delete',upload.fields([]),function(req,res){
   var deletedModel = require('../models/product_delete');
-  var productpid=req.query.productpid;
-  deletedModel.delete(productpid);
-
-
+  deletedModel.delete(req.query.productpid);
 });
 
 // 구매 확정
-router.post('/complete',function(req,res){
-  var complete = require('../models/product_complete');
+router.post('/complete',upload.fields([]),function(req,res){
+  var completeModel = require('../models/product_complete');
+  
 });
 
 module.exports = router;
