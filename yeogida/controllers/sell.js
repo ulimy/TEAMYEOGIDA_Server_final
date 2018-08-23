614b1c6207f76fd893321e271ab07e3b3b7764d4
ulimy <ulim130@naver.com>
2018-08-23 20:04:47 +0900

    sell sold

--- yeogida/controllers/sell.js
+++ yeogida/controllers/sell.js
@@ -1,14 +1,22 @@
 var express = require("express");
 var router = express.Router();
+var multer  = require('multer');
+var upload = multer();
 
 // 내상품
-router.post('/mysell_info',function(req,res){
+router.post('/mysell_info',upload.fields([]),function(req,res){
   var mysellModel = require("../models/sell_mysell_info");
+  mysellModel.info(req.body.personpid).then((data)=>{
+    console.log(data);
+  });
 });
 
 //판매내역
-router.post('/sold_info',function(req,res){
+router.post('/sold_info',upload.fields([]),function(req,res){
   var soldModel = require("../models/sell_sold_info");
+  soldModel.info(req.body.personpid).then((data)=>{
+    console.log(data);
+  });
 });
 
 module.exports = router;
