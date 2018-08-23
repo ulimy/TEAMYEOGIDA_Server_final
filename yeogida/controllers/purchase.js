9990adef1318d5c17a2c354618e66d214d0dae6a
moonjihae <dhfptwlqlc@naver.com>
2018-08-23 20:35:57 +0900

    purchase & search

--- yeogida/controllers/purchase.js
+++ yeogida/controllers/purchase.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var multer=require("multer");
+var upload=multer();
 
 // 구매내역
-router.post('info',function(req,res){
-  var buyinfo = require('../models/purchase_info');
+router.post('/info',upload.fields([]),function(req,res){
+  var purchaseinfoModel = require('../models/purchase_info');
+  var personpid=req.body.personpid;
+  purchaseinfoModel.info(personpid)
+  .then((data)=>{
+      res.json(data);
+    }).then((err)=>{
+      if (err) console.error(err);
+    });
 });
 
 module.exports = router;
