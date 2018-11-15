cbb641c394bbe59c68c380867284a660949307d7
ulimy <ulim130@naver.com>
2018-11-15 19:21:59 +0900

    chatting structure

---
+++ yeogida/controllers/chatting.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var multer=require("multer");
+var upload=multer();
+
+// 채팅방 생성 및 채팅 내용 조회
+router.post('/',upload.fields([]),function(req,res){
+  var chatting = require('../models/chat_chatting');
+  var chatinfo = require('../models/chat_info');
+
+  var personpid=req.body.personpid;
+  var productpid = req.body.productpid;
+});
+
+// 메시지 전송
+router.post('/chat_message',upload.fields([]),function(req,res){
+  var message  = require('../models/message');
+  var chatinfo = require('../models/chat_info');
+});
--- yeogida/controllers/choice.js
+++ yeogida/controllers/choice.js
@@ -4,10 +4,10 @@ var multer = require('multer');
 var upload = multer();
 
 //찜한 상품 리스트 보여주기(판매된거,이미 기한 지난건 x)
-router.post('/info',upload.fields([]),function(req,res){
+router.get('/info',function(req,res){
   var infoModel = require('../models/choice_info');
-  var personpid=req.body.choice_personpid;
-  infoModel.info(personpid)
+
+  infoModel.info(req.query.personpid)
     .then((data)=>{
       res.json(data);
     }).then((err)=>{
@@ -20,18 +20,15 @@ router.post('/info',upload.fields([]),function(req,res){
 router.post('/register',upload.fields([]),function(req,res){
   var registerModel = require('../models/choice_register');
   var register_info=req.body;
-  registerModel.register(register_info).then(()=>{
-    res.json({message:"success"});
-  });
+  registerModel.register(register_info);
+  res.json({message:"success"});
 });
 
 //찜한 상품 productchoice에서 삭제하기
 router.post('/delete',upload.fields([]),function(req,res){
   var deletedModel = require('../models/choice_delete');
   var deleted_info=req.body;
-  deletedModel.delete(deleted_info).then(()=>{
-    res.json({message:"success"});
-  });
+  deletedModel.delete(deleted_info);
 });
 
 module.exports = router;
