92f91eea0e3fc92df1b02f34c56fccdc6e3208ed
ulimy <ulim130@naver.com>
2019-01-10 22:26:05 +0900

    purchase 수정

--- yeogida/controllers/purchase.js
+++ yeogida/controllers/purchase.js
@@ -4,13 +4,12 @@ var router = express.Router();
 // 구매내역
 router.post('/info',function(req,res){
   var purchaseinfoModel = require('../models/purchase_info');
-  var personpid=req.body.buyer_personpid;
 
-  purchaseinfoModel.info(personpid)
+  purchaseinfoModel.info(req.body)
   .then((data)=>{
       res.json(data);
     }).then((err)=>{
-      if (err) console.error(err);
+      if (err) throw(err);
     });
 });
 
